1b742037086aacb4614a1d1bc88d748362048a71
MANO-B <maikegam@ncc.go.jp>
2025-06-14 20:59:38 +0900

    Update __init__.py

--- src/huetracer/__init__.py
+++ src/huetracer/__init__.py
@@ -3,5 +3,3 @@ from .vae import SpatialMicroenvironmentAnalyzer
 from .harmony import run_harmony
 from .plot import plot_all_cell_type_highlights, plot_all_clusters_highlights
 from .cci import make_coexp_cc_df, make_non_zero_values, make_positive_values, make_top_values, safe_toarray
-__version__ = "0.0.1"
-
