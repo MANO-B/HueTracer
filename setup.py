c429d440974e8ad966e8c2368e69e2560ecbdb17
MANO-B <maikegam@ncc.go.jp>
2025-06-14 17:47:05 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='huetracer',
+    version='0.0.1',
+    description='Analyze cell-cell interaction with spatial transcriptome data.',
+    author='Masachika Ikegami',
+    author_email='ikegamitky@gmail.com',
+    url='https://github.com/MANO-B/HueTracer',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'tqdm',
+        'scanpy',
+        'scikit-image',
+        'opencv-python',
+        'pandas',
+        'torch',
+        'plotly',
+        'matplotlib'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.7',
+)
