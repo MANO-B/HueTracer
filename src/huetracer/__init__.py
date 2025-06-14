15534741cb075370a734e5b150c994ca681c1c6e
MANO-B <maikegam@ncc.go.jp>
2025-06-14 22:03:03 +0900

    Update __init__.py

--- src/huetracer/__init__.py
+++ src/huetracer/__init__.py
@@ -1,4 +1,4 @@
-from .statistics import wilson_score_interval, beta_binomial_test_vs_population
+from .statistics import wilson_score_interval, beta_binomial_test_vs_population, calculate_coexpression_coactivity
 from .vae import SpatialMicroenvironmentAnalyzer
 from .harmony import run_harmony
 from .plot import plot_all_cell_type_highlights, plot_all_clusters_highlights
