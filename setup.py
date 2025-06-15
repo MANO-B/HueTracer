01d402cd205182aeb57487a3c93dad97528f727f
ikegami <ikegamitky@gmail.com>
2025-06-15 15:22:48 +0900

    Fix: error fix, v0.0.1 -> v0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='huetracer',
-    version='0.0.1',
+    version='0.0.3',
     description='Analyze cell-cell interaction with spatial transcriptome data.',
     author='Masachika Ikegami',
     author_email='ikegamitky@gmail.com',
